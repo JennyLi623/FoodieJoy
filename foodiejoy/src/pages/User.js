e689087feab2027681318b81c09f0981d653eda8
idodra <iris918919@pku.edu.cn>
2022-03-30 00:50:37 -0400

    User.js Updated
    Updated User.js
    changed User.js, landing.css
    added a new package
    added a picture

--- foodiejoy/src/pages/User.js
+++ foodiejoy/src/pages/User.js
@@ -2,27 +2,40 @@ import React, { Component } from "react";
 import {Link} from 'react-router-dom';
 import {Container, Row, Col, Button} from 'react-bootstrap';
 import foodpost from "./../static/foodpost.jpeg";
+import userpic from "./../static/user.png";
+import ReactRoundedImage from "react-rounded-image";
 
 class User extends Component {
   state = {};
   render() {
     return (
-      <div>
-      <Container>
-        <Row>
-          <Col md="6">
-            <img className="img-fluid postimg" src={foodpost} />
-          </Col>
-          <Col md="6">
-            <p>
-              Name
-            </p>
-            <p>
-              email
-            </p>
-          </Col>
-        </Row>
-      </Container>
+      <div id = "user">
+        <Container >
+          <Row>
+            <Col md="5" >
+              <ReactRoundedImage
+              image={userpic}
+              roundedColor= "#fcf5ed"
+              imageWidth="300"
+              imageHeight="300"
+              roundedSize="5"
+              />
+            </Col>
+            <Col md="5">
+              <p className="username">
+                Name
+              </p>
+              <p className="username">
+                Email
+              </p>
+            </Col>
+          </Row>
+        </Container>
+        <Container>
+          <p className="title">
+            Collected Items
+          </p>
+        </Container>
       </div>
     );
   }
