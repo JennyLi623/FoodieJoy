4f2111af81ef558ebc0f32e41f0c1e67bf2ce7d4
yuan tang <Chat@Chatloys-MacBook-Pro.local>
2022-05-04 22:03:46 -0400

    [T]1.add Search function 2.Refine the layout of Main Page 3.Delete the redundant route

--- foodiejoy/src/pages/SearchBox.js
+++ foodiejoy/src/pages/SearchBox.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import TextField from '@mui/material/TextField';
+import {Container, Row, Col, Button} from 'react-bootstrap';
+import './../css/Banner.css'
 
-const SearchBox = ({searchChange}) => {
+const SearchBox = ({keyword, searchChange}) => {
   return (
-    <input type='search'
-    placeholder='search dishes'
-    onChange={searchChange}/>
+    <div className='searchbox'>
+      <Container>
+        <Row>
+          <Col md="8">
+     <div className='search-box mx-auto'>
+    <input
+      id="search-res"
+      type="text"
+      placeholder=""
+      name="keyword"
+      value={keyword}
+      onChange={searchChange}
+      variant="standard"
+      style={{width: "300px"}}
+    />
+    </div >
+          </Col>
+          <Col md="4">
+          <button
+              onClick={() => window.scrollBy(0, 525)}
+          >
+              Search
+    </button>
+          </Col>
+        </Row>
+      </Container>
+      </div>
+
+    
   );
 }
 
